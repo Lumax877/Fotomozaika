0d738485c33ae486933cc4bfd9da99ec5808945a
Bartosz_Ochtabinski <bochta.binski7@gmail.com>
2023-12-09 19:56:50 +0100

    script now retrieves avg tile color from filename instead of calculating it

--- targetcolorsearchmosaic.py
+++ targetcolorsearchmosaic.py
@@ -1,13 +1,20 @@
-from PIL import Image
 import os
-import random
-from skimage import color
+import re
 import numpy as np
+from PIL import Image
+from skimage import color
 
-# very slow due to tile color calculation
 
 def rgb_to_lab(rgb):
     return color.rgb2lab([[rgb]])[0][0]
+
+
+def extract_color_from_tile_filename(filename):
+    match = re.match(r'image_(\d+)_(\d+)_(\d+)_(\d+)', filename)
+    if match:
+        return tuple(map(int, match.groups()[1:]))
+    return None
+
 
 def calculate_average_color(region):
     width, height = region.size
@@ -24,50 +31,55 @@ def calculate_average_color(region):
     avg_color = (r // total_pixels, g // total_pixels, b // total_pixels)
     return avg_color
 
+
 def color_difference(color1, color2):
     lab1 = rgb_to_lab(color1)
     lab2 = rgb_to_lab(color2)
     return np.linalg.norm(lab1 - lab2)
 
-def find_most_similar_tile(target_color, tile_images):
+
+def find_most_similar_tile(target_color, tile_filenames):
     min_difference = float('inf')
     most_similar_tile = None
 
-    for tile_path in tile_images:
-        tile_image = Image.open(tile_path)
-        tile_color = calculate_average_color(tile_image)
+    for tile_filename in tile_filenames:
+        tile_color = extract_color_from_tile_filename(tile_filename)
 
         difference = color_difference(target_color, tile_color)
 
         if difference < min_difference:
             min_difference = difference
-            most_similar_tile = tile_image
+            most_similar_tile = tile_filename
 
     return most_similar_tile
 
-def random_tile_mosaic(input_image_path, tiles_folder, output_path, tile_size=100):
+
+def mosaic_from_tiles(input_image_path, tiles_folder, output_path, tile_size=100):
     original_image = Image.open(input_image_path)
     original_w, original_h = original_image.size
 
     mosaic_image = Image.new('RGB', (original_w, original_h), (255, 255, 255))
-    tile_images = [os.path.join(tiles_folder, filename) for filename in os.listdir(tiles_folder)]
+
+    tile_filenames = os.listdir(tiles_folder)
 
     for y in range(0, original_h, tile_size):
         for x in range(0, original_w, tile_size):
             region = original_image.crop((x, y, x + tile_size, y + tile_size))
+
             target_color = calculate_average_color(region)
-            print(y*x)
 
-            most_similar_tile = find_most_similar_tile(target_color, tile_images)
+            most_similar_tile_filename = find_most_similar_tile(target_color, tile_filenames)
 
+            most_similar_tile_path = os.path.join(tiles_folder, most_similar_tile_filename)
+            most_similar_tile = Image.open(most_similar_tile_path)
             mosaic_image.paste(most_similar_tile, (x, y))
-
+            print(x*y)
 
     mosaic_image.save(output_path)
 
-random_tile_mosaic(
-    input_image_path="venv/generatedimages/some_input_images/sea1000x1000.jpg",
+mosaic_from_tiles(
+    input_image_path="venv/generatedimages/some_input_images/bridge.jpg",
     tiles_folder="venv/generatedimages/script_v1",
-    output_path="venv/generatedimages/some_input_images/seamosaic.jpg"
+    output_path="venv/generatedimages/some_input_images/bridgemosaic10k.jpg"
 )
 
